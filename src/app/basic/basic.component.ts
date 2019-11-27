a45725db1841fb94acb840c2e914807521231afa
Paulo <paulo.correa@indt.org.br>
2019-11-27 14:13:19 -0400

    create observable to console five

--- src/app/basic/basic.component.ts
+++ src/app/basic/basic.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
+import { nextTick } from 'q';
 
 @Component({
   selector: 'app-basic',
@@ -10,6 +12,19 @@ export class BasicComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const myFirstObservable = new Observable(
+      (observer: Observer<number>) => {
+        observer.next(1); // quem estiver observando esse objeto verá essas infors next(1)...
+        observer.next(2);
+        observer.next(3);
+        observer.next(4);
+        observer.next(5);
+        observer.complete();
+      }
+    );
+    myFirstObservable.subscribe((n: number) => {
+      console.log(n);
+    });
   }
 
 }
