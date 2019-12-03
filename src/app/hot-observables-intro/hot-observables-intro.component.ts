4e97d572482d898fd4ae53bfabee5f359d798681
Paulo <paulo.correa@indt.org.br>
2019-12-03 15:04:51 -0400

    create function Listener 1 and 2

--- src/app/hot-observables-intro/hot-observables-intro.component.ts
+++ src/app/hot-observables-intro/hot-observables-intro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { fromEvent, Observable} from 'rxjs';
+import { Observable, fromEvent, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-hot-observables-intro',
@@ -7,13 +7,13 @@ import { fromEvent, Observable} from 'rxjs';
   styleUrls: ['./hot-observables-intro.component.css']
 })
 export class HotObservablesIntroComponent implements OnInit {
-  
+
   @ViewChild('myButton') button: ElementRef;
 
   n1: number = 0;
   n2: number = 0;
-  s1: string = "";
-  s2: string = "";
+  s1: string = '';
+  s2: string = '';
 
   constructor() { }
 
@@ -22,6 +22,32 @@ export class HotObservablesIntroComponent implements OnInit {
       this.button.nativeElement, 'click');
     myBtnClickObservable.subscribe( (event) => console.log('button cliecked 1'));
     myBtnClickObservable.subscribe( (event) => console.log('button cliecked 2'));
-  }
+    class Producer {
+      private myListeners = [];
+      private n = 0;
+      private id;
 
+      addListener(l) {
+        this.myListeners.push(l);
+        console.log(this.myListeners.length);
+      }
+
+      start() {
+        this.id = setInterval(()=>{
+          this.n++;
+          console.log('From Producer: ' + this.n);
+          for(let l of this.myListeners)
+            l(this.n);//passar um dado
+        }, 1000);
+      }
+      stop() {
+        clearInterval(this.id);
+      }
+    }
+    let producer: Producer = new Producer();
+    producer.start();
+
+    producer.addListener((n) => console.log('From listener 1', n));
+    producer.addListener((n) => console.log('From listener 2', n));
+  }
 }
